d57c86372ec3f31060795529ef45c130c6a8abea
Russell Fisher <russell_fisher@Russells-MacBook-Pro.local>
2017-06-13 12:43:55 -0600

    add path for zipcode

--- api/property.js
+++ api/property.js
@@ -1,28 +1,40 @@
 var request = require("request");
 
 module.exports = {
-    homeInfo: function(addressComponents, addressStuff) {
-        return new Promise((resolve, reject) => {
-            var url = 'http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19l7src3rbf_64j9s&address=' + addressStuff + '+' + addressComponents[1].short_name + '&citystatezip=' + addressComponents[3].long_name + '%2C+' + addressComponents[5].short_name;
+  homeInfo: function(addressComponents, addressStuff) {
+    return new Promise((resolve, reject) => {
+      var url = 'http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19l7src3rbf_64j9s&address=' + addressStuff + '+' + addressComponents[1].short_name + '&citystatezip=' + addressComponents[3].long_name + '%2C+' + addressComponents[5].short_name;
 
-            function callback(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    resolve(body);
-                }
-            }
-            request(url, callback);
-        });
-    },
-    homeInfoByAddress: function(street, city, state) {
-        return new Promise((resolve, reject) => {
-            var url = 'http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19l7src3rbf_64j9s&address=' + street + '&citystatezip=' + city + '%2C+' + state;
+      function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          resolve(body);
+        }
+      }
+      request(url, callback);
+    });
+  },
+  homeInfoByAddress: function(street, city, state) {
+    return new Promise((resolve, reject) => {
+      var url = 'http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19l7src3rbf_64j9s&address=' + street + '&citystatezip=' + city + '%2C+' + state;
 
-            function callback(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    resolve(body);
-                }
-            }
-            request(url, callback);
-        });
-    }
+      function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          resolve(body);
+        }
+      }
+      request(url, callback);
+    });
+  },
+  homeInfoByZipCode: function(street, zipcode) {
+    return new Promise((resolve, reject) => {
+      var url = 'http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19l7src3rbf_64j9s&address=' + street + '&citystatezip=' + zipcode;
+
+      function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          resolve(body);
+        }
+      }
+      request(url, callback);
+    });
+  }
 };
--- routes/index.js
+++ routes/index.js
@@ -8,41 +8,53 @@ var parser = require('xml2json');
 var addressComponents;
 
 router.get('/geocode/json', function(req, res, next) {
-    var addressComponents;
-    api.getAddress(req.query.lat, req.query.lng).then(function(data) {
-        var address = JSON.parse(data);
-        var addressStuff;
-        addressComponents = address.results[0].address_components;
-        if (addressComponents[0].long_name.indexOf('-') !== -1) {
-            addressStuff = addressComponents[0].long_name.split('-')[0];
-        } else {
-            addressStuff = addressComponents[0].long_name;
-        }
-        prop.homeInfo(addressComponents, addressStuff).then(function(data) {
-            var jsonConvert = JSON.parse(parser.toJson(data));
-            var stringifyIt = JSON.stringify(jsonConvert);
-            var replaceErrant = stringifyIt.replace("SearchResults:searchresults", "SearchResults");
-            var reJSON = JSON.parse(replaceErrant);
-            res.json(reJSON);
-        });
+  var addressComponents;
+  api.getAddress(req.query.lat, req.query.lng).then(function(data) {
+    var address = JSON.parse(data);
+    var addressStuff;
+    addressComponents = address.results[0].address_components;
+    if (addressComponents[0].long_name.indexOf('-') !== -1) {
+      addressStuff = addressComponents[0].long_name.split('-')[0];
+    } else {
+      addressStuff = addressComponents[0].long_name;
+    }
+    prop.homeInfo(addressComponents, addressStuff).then(function(data) {
+      var jsonConvert = JSON.parse(parser.toJson(data));
+      var stringifyIt = JSON.stringify(jsonConvert);
+      var replaceErrant = stringifyIt.replace("SearchResults:searchresults", "SearchResults");
+      var reJSON = JSON.parse(replaceErrant);
+      res.json(reJSON);
     });
+  });
 });
 
 router.get('/address/json', function(req, res, next) {
+  if (req.query.city) {
     var street = req.query.street;
     var city = req.query.city;
     var state = req.query.state;
     prop.homeInfoByAddress(street, city, state).then(function(data) {
-        var jsonConvert = JSON.parse(parser.toJson(data));
-        var stringifyIt = JSON.stringify(jsonConvert);
-        var replaceErrant = stringifyIt.replace("SearchResults:searchresults", "SearchResults");
-        var reJSON = JSON.parse(replaceErrant);
-        res.json(reJSON);
+      var jsonConvert = JSON.parse(parser.toJson(data));
+      var stringifyIt = JSON.stringify(jsonConvert);
+      var replaceErrant = stringifyIt.replace("SearchResults:searchresults", "SearchResults");
+      var reJSON = JSON.parse(replaceErrant);
+      res.json(reJSON);
     });
+  } else if (req.query.citystatezip) {
+    var streetAddress = req.query.street;
+    var zipcode = req.query.citystatezip;
+    prop.homeInfoByZipCode(streetAddress, zipcode).then(function(data) {
+      var jsonConvert = JSON.parse(parser.toJson(data));
+      var stringifyIt = JSON.stringify(jsonConvert);
+      var replaceErrant = stringifyIt.replace("SearchResults:searchresults", "SearchResults");
+      var reJSON = JSON.parse(replaceErrant);
+      res.json(reJSON);
+    });
+  }
 });
 
 router.get('/', function(req, res, next) {
-    res.json('test page');
+  res.json('test page');
 });
 
 module.exports = router;
